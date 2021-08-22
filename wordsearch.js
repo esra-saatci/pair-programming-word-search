b1d6dad5588a17ac81e05c366d0810a09464a6af
esra-saatci <esra.saatci86@gmail.com>
2021-08-22 05:45:14 +0000

    Completes pair programming wordSearch exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
 
-module.exports = wordSearch
+  let array = [];
+  for (let i =  0; i < matrix[0].length; i++) {
+    array.push([]);
+  }
+  
+       
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      array[col][row]  = matrix[row][col];
+    }
+  }
+  return array;
+  
+};
+  
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
+
+module.exports = wordSearch;
